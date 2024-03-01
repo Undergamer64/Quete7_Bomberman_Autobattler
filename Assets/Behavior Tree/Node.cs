fbaeecae8735b055d5f185a99c4d03a419849995
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-03-01 14:41:38 +0100

    Tree begin

--- Assets/Behavior Tree/Node.cs
+++ Assets/Behavior Tree/Node.cs
@@ -1,18 +1,79 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.XR;
 
-public class Node : MonoBehaviour
+namespace BehaviorTree
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum NodeState
     {
-        
+        RUNNING,
+        SUCCESS,
+        FAILURE
     }
-
-    // Update is called once per frame
-    void Update()
+    public class Node
     {
-        
+        protected NodeState state;
+
+        public Node parent;
+        protected List<Node> children=new List<Node>();
+
+        private Dictionary<string,object> _dataContext = new Dictionary<string,object>();
+
+        public Node()
+        {
+            parent = null;
+        }
+        public Node(List<Node> children)
+        {
+            foreach (Node node in children)
+            {
+                _Attach(child);
+            }
+            private void _Attach(Node node)
+            {
+                node.parent = this;
+                children.Add(node);
+            }
+        }
+        public virtual NodeState Evaluate() => NodeState.FAILURE;
+
+        public object GetData(string key)
+        {
+            object value = null;
+            if(_dataContext.TryGetValue(key, out value))
+                return value;
+
+            Node node = parent;
+            while (node != null)
+            {
+                value= node.GetData(key);
+                if (value != null)
+                    return value;
+                node = node.parent;
+            }
+            return null;
+        }
+        public bool ClearData(string key)
+        {
+            object value = null;
+            if (_dataContext.ContainsKey(key))
+            {
+                _dataContext.Remove(key);
+                return true;
+            }
+
+            Node node = parent;
+            while (node != null)
+            {
+                bool cleared = node.ClearData(key);
+                if (cleared)
+                    return true;
+                node = node.parent;
+            }
+            return false;
+        }
     }
 }
