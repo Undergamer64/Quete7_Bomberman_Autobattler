37435d21ca05633741d9aa936a1961c9a5cd70ca
SuperPomme2Ter <SuperPomme2Ter@gmail.com>
2024-03-05 16:13:58 +0100

    Move To done(sort of)

---
+++ Assets/Behavior Tree/Neutral/MoveTo.cs
@@ -0,0 +1,30 @@
+using BehaviorTree;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MoveTo : Node
+{
+    private GameObject m_character;
+    public MoveTo(GameObject charRef)
+    {
+        m_character = charRef;
+    }
+
+    
+    public override NodeState Evaluate()
+    {
+        List<S_Tile> path = (List<S_Tile>)GetData("path");
+        if (m_character.GetComponent<S_Character>().MoveToTile(path[0]))
+        {
+            state = NodeState.SUCCESS;
+        }
+
+        state = NodeState.FAILURE;
+        Debug.Log("debut");
+        new WaitForSeconds(0.5f);
+        Debug.Log("fin");
+        return state;
+    }
+}
